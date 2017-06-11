d40c406f271aa710b79e67c3b2ea2b40af761d6e
Thomas Lombart <t.lombart97@gmail.com>
2017-06-11 09:55:37 +0200

    Add french translation

--- routes/user.js
+++ routes/user.js
@@ -21,7 +21,7 @@ router.get('/profile', isLoggedIn, function(req, res, next) {
 
 router.get('/logout', isLoggedIn, function(req, res, next) {
   req.logout();
-  req.flash('info', 'Logged out');
+  req.flash('info', 'Vous êtes maintenant déconnecté');
   res.redirect('/');
 });
 
