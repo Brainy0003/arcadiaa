c67f84af26b7d4cb4e4154564aee5ae5268f03ff
Thomas Lombart <t.lombart97@gmail.com>
2017-06-19 21:15:02 +0200

    Add markdown and improve chat

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,32 +3,43 @@
 $(document).ready(function () {
 
     /* Helpers functions */
-    function addMessage(element, author, content, date) {
+    function addMessage(element, messageClass, author, content, date) {
         moment.locale('fr');
         date = moment(date).format('DD MMM HH:mm');
-        element.append(`<div class="message"><div class="author">${author} <span class='pull-right'>${date}</span></div><div class="content">${content} </div></div>`);
+        element.append(`<div class="${messageClass}"><div class="author">${author} <span class='pull-right'>${date}</span></div><div class="content">${content} </div></div>`);
+        scrollToBottom(element);
+    }
+
+    function scrollToBottom(element) {
         // Scroll to bottom at each message
         let height = element[0].scrollHeight;
         element.scrollTop(height);
     }
 
-    function switchRoom(room) {
-        chat.emit('switchRoom', room);
-    }
 
-    // Get all messages in the database
     function addAllMessages(room, nbMessages) {
         $.get(`/api/messages/${room}`, function (messages) {
             $("#messages").empty();
             let length = messages.length;
             let numberOfMessagesToShow = length > nbMessages ? nbMessages : length;
             for (let i = length - numberOfMessagesToShow; i < length; i++) {
-                addMessage($("#messages"), messages[i].author, messages[i].content, messages[i].date);
+                addMessage($("#messages"), 'message', messages[i].author, messages[i].content, messages[i].date);
             }
         });
     }
 
+    function notifyChat(element, isLeaving, username) {
+        let arrivedOrLeft = isLeaving ? 'a quitté le chat' : 'est arrivé sur le chat';
+        element.append(`<div class="alert alert-info"><p class="text-center">${username} ${arrivedOrLeft}.</p></div>`);
+        scrollToBottom(element);
+    }
+
+    function switchRoom(room) {
+        chat.emit('switchRoom', room);
+    }
+
     var chat = io();
+    addAllMessages('general', 25);
 
     /* Switch on chief room when user clicks on <a> tag */
     $("#chief").on('click', function () {
@@ -40,9 +51,6 @@ $(document).ready(function () {
         switchRoom('general');
     });
 
-    addAllMessages('general', 20);
-
-    chat.emit('room', 'general');
     // Get connected username
     $.get('/api/me', function (data) {
         // Emit username as soon as we have the username
@@ -50,22 +58,36 @@ $(document).ready(function () {
         chat.emit('userConnecting', username);
 
         // Handling events
-        chat.on('connectedUser', function (username) {
-            $("#messages").append(`<div class="alert alert-info"><p class="text-center">${username} s'est connecté.</p></div>`);
+        chat.on('leaveChat', function (username) {
+            notifyChat($("#messages"), true, username);
         });
-        chat.on('disconnectedUser', function (username) {
-            $("#messages").append(`<div class="alert alert-info"><p class="text-center">${username} s'est déconnecté.</p></div>`);
+        chat.on('joinChat', function (username) {
+            notifyChat($("#messages"), false, username);
         });
-        chat.on('chat message', function (message) {
-            addMessage($("#messages"), message.author, message.content, message.date);
+        chat.on('chatMessage', function (message) {
+            addMessage($("#messages"), 'newMessage message', message.author, message.content, message.date);
         });
         chat.on('updatePage', function (room) {
             addAllMessages(room, 20);
         });
+        chat.on('isTyping', function (username) {
+            $("#isTyping").text(`Quelqu'un écrit un message...`);
+        });
+        chat.on('stoppedTyping', function (username) {
+            $("#isTyping").text('');
+        });
+
+        // Emitting events
+        $("#messageInput").on('keyup', function () {
+            chat.emit('isTyping', {
+                username,
+                'message': $("#messageInput").val()
+            });
+        });
 
         // Add new message
         $('form').submit(function () {
-            var messageInput = $("#message");
+            var messageInput = $("#messageInput");
             /* Insert new message if not empty and length < 1000 characters */
             if (messageInput.val().trim() !== '' && messageInput.val().trim().length < 1000) {
                 chat.emit('newMessage', {
--- routes/sockets.js
+++ routes/sockets.js
@@ -1,29 +1,28 @@
 "use strict";
 
+var marked = require('marked');
 var Message = require('../models/message');
 
 module.exports = function (io) {
     io.on('connection', function (socket) {
         socket.on('userConnecting', function (username) {
-            console.log(`${username} is connecting`);
             /* User is initially in the general room */
             socket.room = 'general';
-            socket.join('general');
+            socket.join(socket.room);
             socket.username = username;
-            socket.broadcast.to(socket.room).emit('connectedUser', socket.username);
+            socket.broadcast.to(socket.room).emit('joinChat', username);
         });
         socket.on('disconnect', function () {
-            console.log(`${socket.username} is disconnecting`);
-            socket.broadcast.to(socket.room).emit('disconnectedUser', socket.username);
+            socket.broadcast.to(socket.room).emit('leaveChat', socket.username);
         });
         socket.on('switchRoom', function (newRoom) {
             socket.leave(socket.room);
             socket.join(newRoom);
             socket.emit('updatePage', newRoom);
             socket.room = newRoom;
-
         });
         socket.on('newMessage', function (msg) {
+            msg.content = marked(msg.content);
             msg.room = socket.room;
             msg.date = Date.now();
             let messageToSave = new Message(msg);
@@ -31,28 +30,17 @@ module.exports = function (io) {
                 if (err) {
                     return err;
                 } else {
-                    io.sockets.to(socket.room).emit('chat message', msg);
+                    io.to(socket.room).emit('chatMessage', msg);
                 }
             });
         });
+        socket.on('isTyping', function (data) {
+            if (data.message.length > 0) {
+                socket.broadcast.to(socket.room).emit('isTyping');
+            } else {
+                /* message is empty, so user has stopped typing */
+                socket.broadcast.to(socket.room).emit('stoppedTyping');
+            }
+        });
     });
 };
-
-/* SOCKETS REFERENCE
-
-socket.emit('message', "this is a test"); //sending to sender-client only
-socket.broadcast.emit('message', "this is a test"); //sending to all clients except sender
-socket.broadcast.to('game').emit('message', 'nice game'); //sending to all clients in 'game' room(channel) except sender
-socket.to('game').emit('message', 'enjoy the game'); //sending to sender client, only if they are in 'game' room(channel)
-socket.broadcast.to(socketid).emit('message', 'for your eyes only'); //sending to individual socketid
-io.emit('message', "this is a test"); //sending to all clients, include sender
-io.in('game').emit('message', 'cool game'); //sending to all clients in 'game' room(channel), include sender
-io.of('myNamespace').emit('message', 'gg'); //sending to all clients in namespace 'myNamespace', include sender
-socket.emit(); //send to all connected clients
-socket.broadcast.emit(); //send to all connected clients except the one that sent the message
-socket.on(); //event listener, can be called on client to execute on server
-io.sockets.socket(); //for emiting to specific clients
-io.sockets.emit(); //send to all connected clients (same as socket.emit)
-io.sockets.on() ; //initial connection from a client.
-
-*/
