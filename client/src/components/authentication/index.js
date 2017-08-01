7638bca8c396b79cca511306713fdf8749ecddce
Thomas Lombart <t.lombart97@gmail.com>
2017-08-01 22:19:13 +0200

    Delete GoogleAuth

--- client/src/components/authentication/index.js
+++ client/src/components/authentication/index.js
@@ -1,6 +1,9 @@
 import Signin from './Signin';
 import Signup from './Signup';
-import GoogleAuth from './GoogleAuth';
 import FieldGroup from './FieldGroup';
 
-export { Signin, Signup, GoogleAuth, FieldGroup };
+export {
+    Signin,
+    Signup,
+    FieldGroup
+};
