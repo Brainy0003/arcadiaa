16e956547454b72cc26ac69b88a6af01cdf2466a
Thomas Lombart <t.lombart97@gmail.com>
2017-08-05 16:12:17 +0200

    Redirects if non-chief user access to management

--- client/src/components/management/Management.jsx
+++ client/src/components/management/Management.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import {
     Table,
     TableBody,
@@ -10,52 +13,62 @@ import {
 
 class Management extends Component {
     render() {
-        return (
-            <div>
-                <h4 className="text-center title">Bonjour à vous les chefs ! Cette fonctionnalité n'est pas encore prête... Mais j'y travaille! Patience donc...</h4>
-                {
-                    /*
-                                    <Table>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHeaderColumn>ID</TableHeaderColumn>
-                            <TableHeaderColumn>Name</TableHeaderColumn>
-                            <TableHeaderColumn>Status</TableHeaderColumn>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        <TableRow>
-                            <TableRowColumn>1</TableRowColumn>
-                            <TableRowColumn>John Smith</TableRowColumn>
-                            <TableRowColumn>Employed</TableRowColumn>
-                        </TableRow>
-                        <TableRow>
-                            <TableRowColumn>2</TableRowColumn>
-                            <TableRowColumn>Randal White</TableRowColumn>
-                            <TableRowColumn>Unemployed</TableRowColumn>
-                        </TableRow>
-                        <TableRow>
-                            <TableRowColumn>3</TableRowColumn>
-                            <TableRowColumn>Stephanie Sanders</TableRowColumn>
-                            <TableRowColumn>Employed</TableRowColumn>
-                        </TableRow>
-                        <TableRow>
-                            <TableRowColumn>4</TableRowColumn>
-                            <TableRowColumn>Steve Brown</TableRowColumn>
-                            <TableRowColumn>Employed</TableRowColumn>
-                        </TableRow>
-                        <TableRow>
-                            <TableRowColumn>5</TableRowColumn>
-                            <TableRowColumn>Christopher Nolan</TableRowColumn>
-                            <TableRowColumn>Unemployed</TableRowColumn>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-                */
-                }
-            </div>
-        );
+        if (!this.props.isChief) {
+            return (
+                <Redirect to="/" />
+            );
+        } else {
+            return (
+                <div>
+                    <h4 className="text-center title">Bonjour à vous les chefs ! Cette fonctionnalité n'est pas encore prête... Mais j'y travaille! Patience donc...</h4>
+                    {
+                        /*
+                                        <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHeaderColumn>ID</TableHeaderColumn>
+                                <TableHeaderColumn>Name</TableHeaderColumn>
+                                <TableHeaderColumn>Status</TableHeaderColumn>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableRowColumn>1</TableRowColumn>
+                                <TableRowColumn>John Smith</TableRowColumn>
+                                <TableRowColumn>Employed</TableRowColumn>
+                            </TableRow>
+                            <TableRow>
+                                <TableRowColumn>2</TableRowColumn>
+                                <TableRowColumn>Randal White</TableRowColumn>
+                                <TableRowColumn>Unemployed</TableRowColumn>
+                            </TableRow>
+                            <TableRow>
+                                <TableRowColumn>3</TableRowColumn>
+                                <TableRowColumn>Stephanie Sanders</TableRowColumn>
+                                <TableRowColumn>Employed</TableRowColumn>
+                            </TableRow>
+                            <TableRow>
+                                <TableRowColumn>4</TableRowColumn>
+                                <TableRowColumn>Steve Brown</TableRowColumn>
+                                <TableRowColumn>Employed</TableRowColumn>
+                            </TableRow>
+                            <TableRow>
+                                <TableRowColumn>5</TableRowColumn>
+                                <TableRowColumn>Christopher Nolan</TableRowColumn>
+                                <TableRowColumn>Unemployed</TableRowColumn>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                    */
+                    }
+                </div>
+            );
+        }
     }
 }
+
+const mapStateToProps = (state) => ({ isChief: state.auth.user.isChief });
+
+Management = connect(mapStateToProps)(Management)
 
 export default Management;
