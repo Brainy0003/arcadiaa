60540c4f12e97023d241d65ac4bd10e9208f460a
Thomas Lombart <t.lombart97@gmail.com>
2017-08-05 16:39:19 +0200

    Make secret key... secret

--- server/config/index.js
+++ server/config/index.js
@@ -1,14 +1,7 @@
 const config = {
     'port': process.env.PORT || 3000,
-    'secret': 'mySecretKey',
-    'database': process.env.ARCADIAA_MLAB_URI,
-    auth: {
-        'google': {
-            'clientID': process.env.GOOGLE_CLIENT_ID,
-            'clientSecret': process.env.GOOGLE_CLIENT_SECRET,
-            'callbackURL': 'http://arcadiaa.herokuapp.com/auth/google/callback'
-        }
-    }
+    'secret': process.env.ARCADIAA_SECRET_KEY,
+    'database': process.env.ARCADIAA_MLAB_URI
 }
 
 export default config;
